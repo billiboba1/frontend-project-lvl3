90de88e3e70603ea6d12f8fca2eec6dbc6350c06
billiboba1 <shadrinalexander@mail.ru>
2022-11-17 12:57:56 +0300

    33

--- src/js/build.js
+++ src/js/build.js
@@ -2,7 +2,7 @@ import ru from '../../locales/ru.js';
 import i18next from 'i18next';
 import * as yup from 'yup';
 
-export default build = () => {
+export default () => {
   i18next.init({
     lng: 'ru',
     debug: true,
@@ -15,5 +15,5 @@ export default build = () => {
     mixed: {
       url: 'it should be a valid URL',
     }
-  })
+  });
 };
--- src/js/index.js
+++ src/js/index.js
@@ -2,37 +2,37 @@ import '../scss/style.scss';
 import * as bootstrap from 'bootstrap';
 import onChange from 'on-change';
 import * as yup from 'yup';
-import build from './build';
+import build from './build.js';
 
 const state = {
   url: '',
   error: '',
 };
 
-build.then((result) => {
-  const form = document.querySelector('.rss-form');
-  const watchedState = onChange(state, (previousValue, value) => {
-    validation(value)
-      .then((result) => {
-        if (result) {
-          const newEl = document.createElement('p');
-          newEl.innerHTML = value;
-          document.body.appendChild(newEl);
-          const input = document.querySelector('input');
-          input.classList.remove('border', 'border-danger');
-        } else {
-          const input = document.querySelector('input');
-          input.classList.add('border', 'border-danger');
-        }
-      });
-  });
-  const validation = (string) => {
-    const scheme = yup.string().url();
-    return scheme.isValid(string);
-  };
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    watchedState.url = formData.get('text');
-  });
+build();
+
+const form = document.querySelector('.rss-form');
+const watchedState = onChange(state, (previousValue, value) => {
+  validation(value)
+    .then((result) => {
+      if (result) {
+        const newEl = document.createElement('p');
+        newEl.innerHTML = value;
+        document.body.appendChild(newEl);
+        const input = document.querySelector('input');
+        input.classList.remove('border', 'border-danger');
+      } else {
+        const input = document.querySelector('input');
+        input.classList.add('border', 'border-danger');
+      }
+    });
+});
+const validation = (string) => {
+  const scheme = yup.string().url();
+  return scheme.isValid(string);
+};
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  watchedState.url = formData.get('text');
 });

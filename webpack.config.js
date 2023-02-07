6e47d8fc3d748b0ab66102b1d4cb2379f093388b
billiboba1 <shadrinalexander@mail.ru>
2023-02-07 14:38:47 +0300

    node 16.x

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
@@ -7,58 +5,58 @@ const isProduction = process.env.NODE_ENV == 'production';
 
 
 const config = {
-    entry: './src/js/index.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'index.js',
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-        hot: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
-    ],
-    module: {
-        rules: [
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'index.js',
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
           {
-            test: /\.(scss)$/,
-            use: [
-              {
-                loader: 'style-loader'
-              },
-              {
-                loader: 'css-loader'
-              },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  postcssOptions: {
-                    plugins: () => [
-                      require('autoprefixer')
-                    ]
-                  }
-                }
-              },
-              {
-                loader: 'sass-loader'
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: () => [
+                  require('autoprefixer')
+                ]
               }
-            ]
+            }
+          },
+          {
+            loader: 'sass-loader'
           }
         ]
       }
+    ]
+  }
 };
 
 module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+  if (isProduction) {
+    config.mode = 'production';
+
+
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
